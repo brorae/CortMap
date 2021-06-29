06784db85cc7c9330490ef57f57cef9edd0d3c85
Junhyung-Choi <junhyung9571@naver.com>
2021-06-29 20:55:33 +0900

    ADD data

--- CortMapProj/CortMapApp/views.py
+++ CortMapProj/CortMapApp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render, redirect
 from .models import Rental
+
+import requests
+
 # Create your views here.
 def main(request):
     return render(request,'main.html')
+
+def data(request):
+    headers = {
+        'accept': 'application/json',
+    }
+
+    params = (
+        ('strict', 'true'),
+    )
+
+    response = requests.get('https://disease.sh/v3/covid-19/countries/KOR', headers=headers, params=params)
+
+    #NB. Original query string below. It seems impossible to parse and
+    #reproduce query strings 100% accurately so the one below is given
+    #in case the reproduced version is not "correct".
+    # response = requests.get('https://disease.sh/v3/covid-19/countries/KOR?strict=true', headers=headers)
+    context = {
+        'data' : response,
+        "json": response.text,
+    }
+    return render(request,'data.html',context)
--- CortMapProj/CortMapProj/urls.py
+++ CortMapProj/CortMapProj/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from CortMapApp.views import main
+from CortMapApp.views import main, data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name = "main"),
+    path('data/',data, name = "data")
 ]
