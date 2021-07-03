9e711a5056725a8c5bbe159b3bc2bab22cf82db8
Junhyung-Choi <junhyung9571@naver.com>
2021-07-03 22:17:22 +0900

    FIXING CSS

--- CortMapProj/CortMapApp/views.py
+++ CortMapProj/CortMapApp/views.py
@@ -10,6 +10,14 @@ def main(request):
     }
     return render(request,'main.html',context)
 
+def main1(request):
+    datas = response.json()
+    context = {
+        "CorData": getCountriesInformation(),
+    }
+    return render(request,'main1.html',context)
+
+
 def data(request):
     datas = response.json()
     context = {
